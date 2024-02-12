fca90589ac91a6712c5d10b747eea7956af6bf81
suhail-nagore <mohdsuhail2232@gmail.com>
2024-02-13 00:45:21 +0530

    cloudinary, jwt, multer setup

---
+++ advance/src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp"); // Set the destination folder for uploaded files
+    },
+    filename: function (req, file, cb) {
+      // Set the filename to be unique (e.g., timestamp + original name)
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, uniqueSuffix + "-" + file.originalname);
+    },
+  });
+  
+   export const upload = multer({ storage: storage });
